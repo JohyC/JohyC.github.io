66de12719285330981074b6594547e046a924595
Johy <38210128+JohyC@users.noreply.github.com>
2021-02-20 13:16:42 +0800

    Resume
    Resume

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function loading() {
+  console.log('a')
+  document.getElementsByClassName('avatar')[0].style.display = 'block';
+  document.getElementsByClassName('loading')[0].style.display = 'none';
+}loading();
+
+console.log("感谢您的浏览，期待能够加入贵公司");
