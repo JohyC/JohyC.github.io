2389019377525bea6a5d46ec3272e14023bd98e1
Johy <38210128+JohyC@users.noreply.github.com>
2021-03-20 13:53:43 +0800

    cahnge resume,add TodoApp

---
+++ Practise/TodoApp/js/todo-app.js
@@ -0,0 +1,76 @@
+/**
+ * 过滤todos
+ */
+var filters = {
+  all: function (todos) {
+    return todos;
+  },
+  done: function (todos) {
+    return todos.filter(function (todo) {
+      return todo.completed;
+    });
+  },
+  todo: function (todos) {
+    return todos.filter(function (todo) {
+      return !todo.completed;
+    });
+  },
+};
+// @ts-ignore
+var vue = new Vue({
+  el: ".container",
+  data: {
+    message: "",
+    todos: [
+      {
+        content: "test",
+        completed: false,
+        key: 0,
+      },
+    ],
+    filters: [
+      { text: "全部", value: "all" },
+      { text: "已完成", value: "done" },
+      { text: "未完成", value: "todo" },
+    ],
+    visibility: "all",
+    isActive: 0,
+  },
+  methods: {
+    add: function () {
+      if (this.$data.message !== "") {
+        this.$data.todos.push({
+          content: this.$data.message,
+          key: Date.now()
+        });
+      }
+      this.$data.message = "";
+    },
+    onEnter: function () {
+      this.add();
+    },
+    del: function (index) {
+      this.$data.todos.splice(index, 1);
+    },
+    clickActive: function (filter, index) {
+      debugger
+      this.isActive = index;
+      switch (filter.value) {
+        case "done":
+          // @ts-ignore
+          return this.$data.visibility = "done" 
+        case "todo":
+          // @ts-ignore
+          return this.$data.visibility = "todo"
+        default:
+          // @ts-ignore
+          return this.$data.visibility = "all"
+      }
+    },
+  },
+  computed: {
+    filteredTodos: function () {
+      return filters[this.visibility](this.todos);
+    },
+  },
+});
