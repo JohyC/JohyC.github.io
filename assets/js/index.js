6aba504fac586d6ce8d28e1038f8b62a84231082
Johy <38210128+JohyC@users.noreply.github.com>
2021-03-27 19:04:34 +0800

    change resume

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 function loading() {
-  console.log('a')
   document.getElementsByClassName('avatar')[0].style.display = 'block';
   document.getElementsByClassName('loading')[0].style.display = 'none';
 }loading();
